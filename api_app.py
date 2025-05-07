a81132d1a24d4d44d38db82e7b9b1543b1a8a819
5_vania_5 <antonivan1234567@gmail.com>
2025-05-07 12:37:50 +0300

    Fully-simple worked website for cats facts

--- api_app.py
+++ api_app.py
@@ -6,14 +6,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    url = "https://jsonplaceholder.typicode.com/posts"
+    url = "https://catfact.ninja/fact"
     response = requests.get(url)
     data = response.json()
 
+    # Save data to output.json
     with open("output.json", "w") as f:
         json.dump(data, f, indent=2)
 
-    return render_template("index.html", posts=data[:10])  # Display 10 items
+    return render_template("index.html", fact=data["fact"])
 
 if __name__ == "__main__":
     app.run(debug=True)
